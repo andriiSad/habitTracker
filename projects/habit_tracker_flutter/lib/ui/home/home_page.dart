1982d50333f1340d976dfb0c3b543aa275fc5ba6
Andrii Sadovyi <waterpolomen2@gmail.com>
2022-12-06 11:40:14 +0100

    init

--- projects/habit_tracker_flutter/lib/ui/home/home_page.dart
+++ projects/habit_tracker_flutter/lib/ui/home/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:habit_tracker_flutter/ui/task/animated_task.dart';
+import 'package:habit_tracker_flutter/ui/task/task_completion_ring.dart';
 import 'package:habit_tracker_flutter/ui/theming/app_theme.dart';
 
 class HomePage extends StatelessWidget {
@@ -6,6 +8,12 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppTheme.of(context).primary,
+      body: Center(
+        child: SizedBox(
+          width: 240,
+          child: AnimatedTask(),
+        ),
+      ),
     );
   }
 }
---
+++ projects/habit_tracker_flutter/lib/ui/task/animated_task.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:habit_tracker_flutter/ui/task/task_completion_ring.dart';
+
+class AnimatedTask extends StatefulWidget {
+  const AnimatedTask({Key? key}) : super(key: key);
+
+  @override
+  State<AnimatedTask> createState() => _AnimatedTaskState();
+}
+
+class _AnimatedTaskState extends State<AnimatedTask>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _animationController;
+  late final Animation<double> _curveAnimation;
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 750),
+    );
+    _curveAnimation =
+        _animationController.drive(CurveTween(curve: Curves.easeInOut));
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  void _handleTapDown(TapDownDetails details) {
+    if (_animationController.status != AnimationStatus.completed) {
+      _animationController.forward();
+    } else {
+      _animationController.value = 0.0;
+    }
+  }
+
+  void _handleTapUp(TapUpDetails details) {
+    if (_animationController.status != AnimationStatus.completed) {
+      _animationController.reverse();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTapDown: _handleTapDown,
+      onTapUp: _handleTapUp,
+      child: AnimatedBuilder(
+        animation: _curveAnimation,
+        builder: (BuildContext context, Widget? child) => TaskCompletionRing(
+          progress: _curveAnimation.value,
+        ),
+      ),
+    );
+  }
+}
