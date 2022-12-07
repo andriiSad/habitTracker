46aef9536399965aeaafd0afa85c096421f45330
Andrii Sadovyi <waterpolomen2@gmail.com>
2022-12-07 15:07:23 +0100

    task completion animation done

--- projects/habit_tracker_flutter/lib/ui/home/home_page.dart
+++ projects/habit_tracker_flutter/lib/ui/home/home_page.dart
@@ -2,7 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:habit_tracker_flutter/constants/app_assets.dart';
 import 'package:habit_tracker_flutter/ui/task/animated_task.dart';
 import 'package:habit_tracker_flutter/ui/task/task_completion_ring.dart';
+import 'package:habit_tracker_flutter/ui/task/task_with_name.dart';
 import 'package:habit_tracker_flutter/ui/theming/app_theme.dart';
+
+import '../../models/task_preset.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -12,8 +15,11 @@ class HomePage extends StatelessWidget {
       body: Center(
         child: SizedBox(
           width: 240,
-          child: AnimatedTask(
-            iconName: AppAssets.dog,
+          child: TaskWithName(
+            task: TaskPreset(
+              name: 'Meditate',
+              iconName: AppAssets.meditation,
+            ),
           ),
         ),
       ),
---
+++ projects/habit_tracker_flutter/lib/ui/task/task_with_name.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:habit_tracker_flutter/models/task_preset.dart';
+import 'package:habit_tracker_flutter/ui/task/animated_task.dart';
+
+import '../../constants/text_styles.dart';
+import '../theming/app_theme.dart';
+
+class TaskWithName extends StatelessWidget {
+  const TaskWithName({
+    Key? key,
+    required this.task,
+  }) : super(key: key);
+
+  final TaskPreset task;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        AnimatedTask(
+          iconName: task.iconName,
+        ),
+        SizedBox(
+          height: 8,
+        ),
+        Text(
+          task.name.toUpperCase(),
+          textAlign: TextAlign.center,
+          style: TextStyles.taskName.copyWith(
+            color: AppTheme.of(context).accent,
+          ),
+        )
+      ],
+    );
+  }
+}
