abed45ee1914a71df9be8bf87ccaa680a1b2afcf
Andrii Sadovyi <waterpolomen2@gmail.com>
2022-12-12 14:22:30 +0100

    added grid

--- projects/habit_tracker_flutter/lib/ui/home/home_page.dart
+++ projects/habit_tracker_flutter/lib/ui/home/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:habit_tracker_flutter/constants/app_assets.dart';
+import 'package:habit_tracker_flutter/ui/home/tasks_grid.dart';
 import 'package:habit_tracker_flutter/ui/task/animated_task.dart';
 import 'package:habit_tracker_flutter/ui/task/task_completion_ring.dart';
 import 'package:habit_tracker_flutter/ui/task/task_with_name.dart';
@@ -12,15 +13,20 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppTheme.of(context).primary,
-      body: Center(
-        child: SizedBox(
-          width: 240,
-          child: TaskWithName(
-            task: TaskPreset(
-              name: 'Meditate',
-              iconName: AppAssets.meditation,
-            ),
-          ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 24,
+          vertical: 16,
+        ),
+        child: TasksGrid(
+          tasks: const [
+            TaskPreset(name: 'Eat a Healthy Meal', iconName: AppAssets.carrot),
+            TaskPreset(name: 'Walk the Dog', iconName: AppAssets.dog),
+            TaskPreset(name: 'Do Some Coding', iconName: AppAssets.html),
+            TaskPreset(name: 'Meditate', iconName: AppAssets.meditation),
+            TaskPreset(name: 'Do 10 Pushups', iconName: AppAssets.pushups),
+            TaskPreset(name: 'Sleep 8 Hours', iconName: AppAssets.rest),
+          ],
         ),
       ),
     );
