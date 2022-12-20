04d926ff8621920eee3bff13e371c2e2cedd7c3f
Andrii Sadovyi <waterpolomen2@gmail.com>
2022-12-20 11:22:27 +0100

    updated page_flip_builder

--- projects/habit_tracker_flutter/lib/ui/home/home_page.dart
+++ projects/habit_tracker_flutter/lib/ui/home/home_page.dart
@@ -2,17 +2,30 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:habit_tracker_flutter/models/task.dart';
 import 'package:habit_tracker_flutter/persistence/hive_data_store.dart';
+import 'package:habit_tracker_flutter/ui/home/page_flip_builder.dart';
 import 'package:habit_tracker_flutter/ui/home/tasks_grid_page.dart';
 import 'package:hive/hive.dart';
 
 class HomePage extends ConsumerWidget {
+  final _pageFlipKey = GlobalKey<PageFlipBuilderState>();
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final dataStore = ref.watch(dataStoreProvider);
-    return ValueListenableBuilder(
-      valueListenable: dataStore.frontTasksListenable(),
-      builder: (_, Box<Task> box, __) => TasksGridPage(
-        tasks: box.values.toList(),
+    return PageFlipBuilder(
+      key: _pageFlipKey,
+      frontBuilder: (_) => ValueListenableBuilder(
+        valueListenable: dataStore.frontTasksListenable(),
+        builder: (_, Box<Task> box, __) => TasksGridPage(
+          tasks: box.values.toList(),
+          onFlip: () => _pageFlipKey.currentState?.flip(),
+        ),
+      ),
+      backBuilder: (_) => ValueListenableBuilder(
+        valueListenable: dataStore.backTasksListenable(),
+        builder: (_, Box<Task> box, __) => TasksGridPage(
+          tasks: box.values.toList(),
+          onFlip: () => _pageFlipKey.currentState?.flip(),
+        ),
       ),
     );
   }
